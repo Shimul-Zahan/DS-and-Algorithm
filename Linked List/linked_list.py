d59a0dd88c70333ca0e2b02ff3cf92e005ad59f0
AlgoWebBot <shimulzahan636@gmail.com>
2024-10-06 03:18:16 +0600

    Create two class and constructor for the Linked list and create node

--- Linked List/linked_list.py
+++ Linked List/linked_list.py
@@ -1,1 +1,20 @@
-print('Linked list')
+#create a new class for create node
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+# create a new class for new node
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+        
+
+# create a object for LinkedList class
+my_linked_list = LinkedList(5)
+
+# print the value
+print(my_linked_list.head.value)
