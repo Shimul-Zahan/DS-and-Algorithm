c63dd1b08347165343511381a016ce384b906b8a
AlgoWebBot <shimulzahan636@gmail.com>
2024-10-06 03:46:26 +0600

    create a append method

--- Linked List/linked_list.py
+++ Linked List/linked_list.py
@@ -11,14 +11,38 @@ class LinkedList:
         self.head = new_node
         self.tail = new_node
         self.length = 1
-    
+   
+    # printing the linked list
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next    
+
+        
     # Create methods
-    def append(self, value):     
-    def prepend(self, value):     
-    def inser(self, value):     
-
+    def append(self, value): 
+        new_node = Node(value)
+        if self.head is None:
+            # print('function is call')
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+        
+    # def prepend(self, value): 
+        
+    # def inser(self, value): 
+    
+    
 # create a object for LinkedList class
-my_linked_list = LinkedList(5)
+my_linked_list = LinkedList(0)
+for i in range(0, 5):
+    if i == 5:
+        continue
+    my_linked_list.append(i+1)
 
-# print the value
-print(my_linked_list.head.value)
+my_linked_list.print_list()
