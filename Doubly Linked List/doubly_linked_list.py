af6a3975adc1b41ef50546427cc5b4616c9b5107
AlgoWebBot <shimulzahan636@gmail.com>
2024-10-08 23:06:35 +0600

    add dubly link list

--- Doubly Linked List/doubly_linked_list.py
+++ Doubly Linked List/doubly_linked_list.py
@@ -1,1 +1,54 @@
-print('doubly linked list')
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+        
+class DoublyLinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+        
+    def print(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev =  self.tail
+            self.tail = new_node
+        self.length += 1
+        return True
+    
+    def pop(self):
+        if self.length == 0:
+            return None
+        temp = self.tail
+        if self.length ==1:
+            self.head = None
+            self.tail = None
+        else:
+            self.tail = self.tail.prev
+            self.tail.next = None
+            temp.prev = None
+            self.length -= 1
+        return temp
+            
+dll = DoublyLinkedList(10)
+for i in range(0, 5):
+        dll.append(i+1)
+    
+dll.print()
+dll.pop()
+dll.print()
+dll.pop()
+dll.print()
