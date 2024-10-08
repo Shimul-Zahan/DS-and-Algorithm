fde24f8d1cb0ca50ed3a375692a07a023c623dac
AlgoWebBot <shimulzahan636@gmail.com>
2024-10-08 23:57:03 +0600

    complete the doubly linked list

--- Doubly Linked List/doubly_linked_list.py
+++ Doubly Linked List/doubly_linked_list.py
@@ -42,11 +42,96 @@ class DoublyLinkedList:
             temp.prev = None
             self.length -= 1
         return temp
-            
+     
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+        self.length += 1
+        return True 
+    
+    def pop_first(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        if self.length ==1:
+            self.head = None
+            self.tail = None
+        else:
+            self.head = self.head.next
+            self.head.prev = None
+            temp.next = None
+            self.length -= 1
+        return temp
+    
+    def get(self, index):
+        if index < 0 and index > self.length:
+            return None
+        temp = self.head
+        if index < self.length / 2:
+            # print("loop - 1")
+            for _ in range(index):
+                temp = temp.next
+        else:
+            # print("loop - 2")
+            temp = self.tail
+            for _ in range(self.length - 1, index, -1):
+                temp = temp.prev
+        return temp
+        
+        
+    def insert(self, index, value):
+        if index < 0 and index > self.length:
+            return False
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
+        
+        new_node = Node(value)
+        before = self.get(index - 1)
+        after = before.next
+        new_node.prev = before
+        new_node.next = after
+        before.next = new_node
+        after.prev = new_node
+        self.length += 1
+        return True
+        
+    def remove(self, index):
+        if index < 0 and index > self.length:
+            return False
+        if index == 0:
+            return self.pop_first()
+        if index == self.length - 1 :
+            return self.pop()
+        
+        temp = self.get(index)
+        before = temp.prev
+        after = temp.next
+        before.next = after
+        after.prev = before
+        temp.prev = None
+        temp.next = None
+        self.length -= 1
+        return True
+          
 dll = DoublyLinkedList(10)
 for i in range(0, 5):
         dll.append(i+1)
-    
+
+print("Remove element: ", dll.remove(2))
+print(dll.get(2))
+print(dll.get(3))
+dll.prepend(20)
+dll.insert(3, 30)
+dll.print()
+dll.pop_first()
 dll.print()
 dll.pop()
 dll.print()
