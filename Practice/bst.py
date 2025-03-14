049a32a313b2366d136655fd879771638238dc86
Xing Ping <112884750+Shimul-Zahan@users.noreply.github.com>
2025-03-15 03:03:35 +0600

    Again start learning DSA

--- Practice/bst.py
+++ Practice/bst.py
@@ -4,11 +4,11 @@ class Node:
         self.left=None
         self.right=None
         
-class BinartTree:
+class BinaryTree:
     def __init__(self):
         self.root=None
         
-    def inser(self, value):
+    def insert(self, value):
         new_node = Node(value)
         if self.root is None:
             self.root = new_node
@@ -28,4 +28,28 @@ class BinartTree:
                     return True
                 temp = temp.right
                 
+    def contains(self, value):
+        if self.root is None:
+            return False
+        temp = self.root
+        while temp:
+            if value < temp.value:
+                temp = temp.left
+            elif value > temp.value:
+                temp = temp.right
+            else: return True
+        return False
+    
+    def inorder(self):   # Left → Root → Right
+        if self.root is not None:
+            self.inorder(self.root.left)
+            print()
                 
+                
+my_tree=BinaryTree()
+my_tree.insert(50)
+my_tree.insert(20)
+my_tree.insert(40)
+my_tree.insert(10)
+my_tree.insert(21)
+print(my_tree.contains(50))
